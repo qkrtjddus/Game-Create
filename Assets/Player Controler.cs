7dfe73085031d0c621ad3a26f9da9d78ffcd6737
kg7736 <kg7736@gmail.com>
2023-08-27 22:18:26 +0900

    유니티 이동3
    유니티 이동3

--- Assets/Player Controler.cs
+++ Assets/Player Controler.cs
@@ -7,20 +7,31 @@ public class PlayerControler : MonoBehaviour
     public Vector2 Inputvec;
     public float speed;
     Rigidbody2D rigid;
+    SpriteRenderer sprite;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
     void Update()
     {
         Inputvec.x = Input.GetAxisRaw("Horizontal");    // A, D 키로 이동
         Inputvec.y = Input.GetAxisRaw("Vertical");       // W, S 키로 이동
-        
+
     }
     private void FixedUpdate()
     {
         Vector2 nextvec = Inputvec.normalized * speed * Time.fixedDeltaTime;
-        rigid.MovePosition(rigid.position + nextvec);
+        rigid.MovePosition(rigid.position + nextvec);  //
     }
+
+    private void LateUpdate()
+    {
+        if (Inputvec.x != 0) 
+        {
+            sprite.flipX = Inputvec.x < 0;
+        }
+    }
+
 }
